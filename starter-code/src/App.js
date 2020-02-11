66bd19819f103615a389bc9553ccccba146235a2
Sadigova Farida <55064917+ironhacker-sadigova@users.noreply.github.com>
2020-02-11 21:26:40 +0100

    not completed

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,170 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import React, { Component } from "react";
+import 'bulma/css/bulma.css';
+import "./App.css";
+import foods from "./foods.json";
+import FoodBox from "./Components/FoodBox";
 
 class App extends Component {
+  state = {
+    calories: 0
+  };
   render() {
+    const { calories } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+        <h1 className="title">IronNutrition</h1>
+          <div>
+          <input className="input search-bar" name="search" placeholder="Search" value=""/>          </div>
+           <div className="columns">
+            <div className="column">
+            <div class="box"></div>
+              {foods.map((food, index) => (
+                <FoodBox key={index} {...food} />
+              ))}
+            </div>
+            <div className="column content">
+              <div className="has-text-left">
+                <strong>Total:{calories} calories</strong>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+/*
+
+     
+          div className="container">
+      <h1 className="title">IronNutrition</h1>
+      <div>
+        <input className="input search-bar" name="search" placeholder="Search" value="">
+      </div>
+      <div className="columns">
+        <div className="column">
+          <div className="box">
+            <article className="media">
+              <div class="media-left">
+                <figure className="image is-64x64">
+                  <img src="https://i.imgur.com/eTmWoAN.png">
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong>Pizza</strong> <br>
+                    <small>400 cal</small>
+                  </p>
+                </div>
+              </div>
+              <div class="media-right">
+                <div class="field has-addons">
+                  <div class="control">
+                    <input type="number" class="input" value="1">
+                  </div>
+                  <div class="control">
+                    <button class="button is-info">+</button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+          <div class="box">
+            <article class="media">
+              <div class="media-left">
+                <figure class="image is-64x64">
+                  <img src="https://i.imgur.com/DupGBz5.jpg">
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong>Salad</strong> <br>
+                    <small>150 cal</small>
+                  </p>
+                </div>
+              </div>
+              <div class="media-right">
+                <div class="field has-addons">
+                  <div class="control">
+                    <input type="number" class="input" value="1">
+                  </div>
+                  <div class="control">
+                    <button class="button is-info">+</button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+          <div class="box">
+            <article class="media">
+              <div class="media-left">
+                <figure class="image is-64x64">
+                  <img src="https://i.imgur.com/hGraGyR.jpg">
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong>Sweet Potato</strong> <br>
+                    <small>120 cal</small>
+                  </p>
+                </div>
+              </div>
+              <div class="media-right">
+                <div class="field has-addons">
+                  <div class="control">
+                    <input type="number" class="input" value="1">
+                  </div>
+                  <div class="control">
+                    <button class="button is-info">+</button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+          <div className="box">
+            {foods.map((food, index) => (
+                <FoodBox key={index} {...food} />
+              ))}
+              </div>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong>Gnocchi</strong> <br>
+                    <small>500 cal</small>
+                  </p>
+                </div>
+              </div>
+              <div class="media-right">
+                <div class="field has-addons">
+                  <div class="control">
+                    <input type="number" class="input" value="1">
+                  </div>
+                  <div class="control">
+                    <button class="button is-info">+</button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div class="column content">
+          <h2 class="subtitle">Today's foods</h2>
+          <ul>
+            <li>1 Pizza = 400 cal</li>
+            <li>2 Salad = 300 cal</li>
+          </ul>
+          <strong>Total: 700 cal</strong>
+        </div>
+      </div>
+    </div>
+  </div>
+*/
---
+++ starter-code/src/Components/FoodBox.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const FoodBox = (props) => {
+    // let awardText;
+    // if(props.hasOscars){
+    //     if(props.IMDbRating >= 9){
+    //         awardText = <p> WOW! Oscar Award and IMDb rating {props.IMDbRating}! </p>
+    //     } else if(props.IMDbRating >= 7){
+    //         awardText = <p> Got the Oscar Award and has IMDb rating {props.IMDbRating}, not bad at all! </p>
+    //     }
+    // } else {
+    //     awardText = <p> Great movie but no Oscars! Has IMDb rating {props.IMDbRating}. </p>
+    // }
+    return (
+    <div className="box">
+        <article className="media">
+            <div className="media-left">
+            <figure className="image is-64x64">
+                <img src={props.image} />
+            </figure>
+            </div>
+            <div className="media-content">
+            <div className="content">
+                <p>
+                <strong>{props.name}</strong> <br />
+                <small>Cal:{props.calories}</small>
+                </p>
+            </div>
+            </div>
+            <div className="media-right">
+            <div className="field has-addons">
+                <div className="control">
+                    <p>Quantity: {props.quantity}</p>
+                    <p>Total Cal: {props.calories * props.quantity}</p>
+                </div>               
+                        
+            </div>
+            <button className="button is-info" onClick={props.clickToDelete}>Delete</button>
+            </div>
+        </article>
+    </div>
+    )
+};
+
+export default FoodBox;
+
+
